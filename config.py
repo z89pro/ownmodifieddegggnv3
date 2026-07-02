7950965b14654cb40bbc047a10651494bca45216
z89pro <z89459377@gmail.com>
2026-07-02 10:07:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,11 +21,11 @@ try:
     API_ID = int(os.getenv("API_ID", "0"))
 except ValueError:
     print("⚠️ API_ID INVALID! Must be an integer.")
-    API_ID = 0
+    API_ID = 27298762
 
-API_HASH     = os.getenv("API_HASH", "")
-BOT_TOKEN    = os.getenv("BOT_TOKEN", "")
-MONGO_DB     = os.getenv("MONGO_DB", "")
+API_HASH     = os.getenv("API_HASH", "ed1afeb86effb107b287d5d3aeaefe4a")
+BOT_TOKEN    = os.getenv("BOT_TOKEN", "8428805859:AAFlf5Rqvu-fCoqOy5dhoUh5QZQk9K78RiI")
+MONGO_DB     = os.getenv("MONGO_DB", "mongodb+srv://defet54636_db_user:XmIjdhGyimZuak59@cluster0.xepvgb3.mongodb.net/?appName=Cluster0")
 DB_NAME      = os.getenv("DB_NAME", "telegram_downloader")
 
 # ─── OWNER / CONTROL SETTINGS ───────────────────────────────────────────────────
@@ -33,19 +33,19 @@ owner_id_str = os.getenv("OWNER_ID", "")
 OWNER_ID     = list(map(int, owner_id_str.split())) if owner_id_str else []
 
 STRING       = os.getenv("STRING", None)
-LOG_GROUP    = int(os.getenv("LOG_GROUP", "-1001234456"))
-FORCE_SUB    = int(os.getenv("FORCE_SUB", "-10012345567"))
+LOG_GROUP    = int(os.getenv("LOG_GROUP", "-1004426994491"))
+FORCE_SUB    = int(os.getenv("FORCE_SUB", "-1004428543058"))
 
 # ─── SECURITY KEYS ──────────────────────────────────────────────────────────────
 MASTER_KEY   = os.getenv("MASTER_KEY", "QWeCQl7F91DPGUdimsgic7_KwAKSx-NgQ8CAVBWaac8=")
 IV_KEY       = os.getenv("IV_KEY", "HOl_k8Dypv6cG_6nIyYbxg==")
 
 # ─── USAGE LIMITS ───────────────────────────────────────────────────────────────
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT  = int(os.getenv("PREMIUM_LIMIT", "500"))
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "10000"))
+PREMIUM_LIMIT  = int(os.getenv("PREMIUM_LIMIT", "50000000"))
 
 # ─── UI / LINKS ─────────────────────────────────────────────────────────────────
-JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro")
+JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/+ATllYz3gW01lYzM9")
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
 
 # Proxy Support
